e182b84633a22073b891b556575247db7c06bd25
shatha163 <shazaalastal2004@gmail.com>
2026-05-13 10:50:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def AI_insights():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
